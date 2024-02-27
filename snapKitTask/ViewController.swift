22f5381d7df0414cecc039a1de99fc3c461a24dd
Saleh <saleh.binessa@gmail.com>
2024-02-27 15:34:01 +0300

    Initial Commit

---
+++ snapKitTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snapKitTask
+//
+//  Created by Saleh Bin Essa on 27/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
